c8a21cdcd38ade66775368c0830e7b44cf535b2e
KrishMaheshwariDev <krishmaheshwari197903@gmail.com>
2026-08-07 16:36:38 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import tailwindcss from "@tailwindcss/vite"
+
+function App() {
+    return (
+      <div className="min-h-screen bg-black flex items-center justify-center">
+            <h1 className="text-6xl font-bold text-red-500">
+                Tailwind Works
+            </h1>
+      </div>
+    )
+}
+
+export default App
